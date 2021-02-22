e651393a7b8ad1968e6d5cdfb4d80526e0586121
Andrew Graham <andrew.graham@digital.homeoffice.gov.uk>
2021-02-22 12:35:32 +0000

    fix typo

--- src/main/java/com/example/tool/controllers/UnitController.java
+++ src/main/java/com/example/tool/controllers/UnitController.java
@@ -1,12 +1,17 @@
 package com.example.tool.controllers;
 
+import com.example.tool.dto.UnitDto;
+import com.example.tool.service.UnitService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Set;
+
 
 @RestController
-public classUnitController {
+public class UnitController {
     @Autowired
     UnitService unitService;
 
